72383f98ffa4e13718af3e5bb7bd6cc6a1f61378
Lilian BAUDRY <lilian.baudry21@gmail.com>
2024-01-16 19:08:54 +0100

    feat: added prisma

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,3 @@
 import Elysia from 'elysia';
-import { usersRoute } from '@/routes/users';
 
 export const app = new Elysia();
-
-app.group('/api/v1', (app) => app.use(usersRoute));
--- src/routes/users.ts
+++
@@ -1,8 +0,0 @@
-import type { Elysia } from 'elysia';
-
-export const usersRoute = (app: Elysia) =>
-  app.group('/users', (app) =>
-    app
-      .get('/', () => '🚀 Hello World')
-      .get('/:id', ({ params }) => `🚀 Hello ${params.id}`),
-  );
